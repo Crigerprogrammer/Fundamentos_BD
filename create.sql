fbfadb2fde75e2a8876778283ef66c21c0898fae
Cristian <crigerprogrammer@gmail.com>
2021-01-17 18:09:36 -0600

    cambios

--- create.sql
+++ create.sql
@@ -8,9 +8,9 @@ USE DATABASE test_db;
 -- CREATE TABLE
 
 CREATE TABLE people(
-    id_person INT,
+    id_person INT PRIMARY KEY NOT NULL IDENTITY(1,1),
     last_name VARCHAR(255),
     first_name VARCHAR(255),
-    address VARCHAR(255),
+    adress VARCHAR(255),
     city VARCHAR(255)
 );
