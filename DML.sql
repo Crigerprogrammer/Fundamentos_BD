83d6b8f8ec3e9803cfa8f7f301e514c1ed3186f0
Cristian <crigerprogrammer@gmail.com>
2021-01-18 10:21:11 -0600

    UPDATE

--- DML.sql
+++ DML.sql
@@ -1,3 +1,15 @@
 -- INSERT
 INSERT INTO people(last_name, first_name, adress, city)
 VALUES ('Hernandez', 'Cristian', 'Villaflores', 'Guatemala')
+
+-- UPDATE
+UPDATE people
+SET last_name = 'Barrios', city = 'Ciudad'
+WHERE id_person = 1;
+
+UPDATE people
+SET first_name = 'Gerardo'
+WHERE city = 'Ciudad';
+
+UPDATE people
+SET first_name = 'Juan';
