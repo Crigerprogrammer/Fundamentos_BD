4b9cb1c9b6fe8e06f842fb3e5f8e6b08aa9bdda5
Cristian <crigerprogrammer@gmail.com>
2021-01-24 10:59:22 -0600

    where 2

--- where.sql
+++ where.sql
@@ -2,3 +2,18 @@
 SELECT *
 FROM posts
 WHERE id < 50;
+
+-- Filtrar por estatus
+SELECT * 
+FROM posts
+WHERE estatus = 'activo';
+
+-- Operador != Quiere que decir que no sea igual
+SELECT *
+FROM posts
+WHERE id != 50;
+
+-- Propiedad LIKE
+SELECT *
+FROM posts
+WHERE titulo LIKE '%escandalo%';
